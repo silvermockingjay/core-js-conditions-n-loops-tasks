51bc94fd32bb174215613ea2be882f2917f0bc30
anakdrch <ana.kudrevich@gmail.com>
2025-01-02 02:11:37 +0100

    feat: implement isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -222,8 +222,13 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let reverse = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reverse += str[i];
+  }
+  if (str === reverse) return true;
+  return false;
 }
 
 /**
